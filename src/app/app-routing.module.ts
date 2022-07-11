f000561c7d6e6e28a56269b465464bcc4f5b6ca1
JPampou <janis@pampoukidis.de>
2022-07-11 08:50:17 +0200

    404 Fehler beheben

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes, routerOptions)],
+  imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 
 })
