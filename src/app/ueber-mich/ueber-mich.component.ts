c48ee06df34c75e7a2f3cf963b2e3e4ecfaab980
JPampou <janis@pampoukidis.de>
2022-10-08 08:20:28 +0200

    Über mich JSON

--- src/app/ueber-mich/ueber-mich.component.ts
+++ src/app/ueber-mich/ueber-mich.component.ts
@@ -12,7 +12,10 @@ export class UeberMichComponent implements OnInit {
   constructor(public _sendMail: SendMailService, public translate: TranslateConfigService) { }
   objectDE: Object =  { "Janis": { "vorname": "Janis", "nachname": "Pampoukidis", "alter": 19, "ausbildung": { "als": "Fachinformatiker für Anwendungsentwicklung", "firma": "LEW Verteilnetz GmbH" } } };
   objectEN: Object =  { "Janis": { "firstname": "Janis", "lastname": "Pampoukidis", "age": 19, "training": { "as": "Developer", "company": "LEW Verteilnetz GmbH" } } };
-
+  
+  objectDEeng: Object =  { "Janis": { "vorname": "Janis", "nachname": "Pampoukidis", "alter": 19, "ausbildung": { "als": "FIA ", "firma": "LVN" } } };
+  objectENeng: Object =  { "Janis": { "firstname": "Janis", "lastname": "Pampoukidis", "age": 19, "training": { "as": "Developer", "company": "LVN" } } };
+  
   ngOnInit() {
   }
 }
